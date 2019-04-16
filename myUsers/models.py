a620634f4af825023d0a4fd589e39286b46cda72
itolog <paukanlove@gmail.com>
2019-04-16 23:24:48 +0300

    db4

--- myUsers/models.py
+++ myUsers/models.py
@@ -1,20 +1,20 @@
-from django.db import models
-from django.contrib.auth.models import User
-from PIL import Image
-
-# Create your models here.
-class Profile(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
-    img = models.ImageField(default='default.png', upload_to='images')
-
-    def __str__(self):
-        return f"Проифиль пользователя {self.user.username}"
-
-    def save(self, *args, **kwargs):
-        super(Profile, self).save(*args, **kwargs)
-
-        image = Image.open(self.img.url)
-        if image.height > 512 or image.width > 512:
-            size = (512, 512)
-            image.thumbnail(size)
-            image.save()
+# from django.db import models
+# from django.contrib.auth.models import User
+# from PIL import Image
+#
+# # Create your models here.
+# class Profile(models.Model):
+#     user = models.OneToOneField(User, on_delete=models.CASCADE)
+#     img = models.ImageField(default='default.png', upload_to='images')
+#
+#     def __str__(self):
+#         return f"Проифиль пользователя {self.user.username}"
+#
+#     def save(self, *args, **kwargs):
+#         super(Profile, self).save(*args, **kwargs)
+#
+#         image = Image.open(self.img.url)
+#         if image.height > 512 or image.width > 512:
+#             size = (512, 512)
+#             image.thumbnail(size)
+#             image.save()
