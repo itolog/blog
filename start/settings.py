8380075effddefacf47672f52b48c5a29ca7ff52
itolog <paukanlove@gmail.com>
2019-04-16 15:57:32 +0300

    make it better11

--- start/settings.py
+++ start/settings.py
@@ -82,7 +82,8 @@ DATABASES = {
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
-DATABASES['default'] = dj_database_url.config()
+db_up = dj_database_url.config()
+DATABASES['default'].update(db_up)
 # Password validation
 # https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
 
