6fa002f944da961284b76d117fd320f9bea10209
itolog <paukanlove@gmail.com>
2019-04-17 12:22:33 +0300

    First version of application moved into github

--- start/settings.py
+++ start/settings.py
@@ -20,10 +20,12 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '4vu3n_*(a8mr&4v)b$@9y!xi76^-bj+0q!yn+&$sd68i0hv@a0'
+# SECRET_KEY = '4vu3n_*(a8mr&4v)b$@9y!xi76^-bj+0q!yn+&$sd68i0hv@a0'
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', '4vu3n_*(a8mr&4v)b$@9y!xi76^-bj+0q!yn+&$sd68i0hv@a0')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+# DEBUG = True
+DEBUG = bool( os.environ.get('DJANGO_DEBUG', True) )
 
 ALLOWED_HOSTS = ['itologjs-blog.herokuapp.com', 'localhost', '127.0.0.1']
 
